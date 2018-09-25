c1826ba1f335c98af46478e7907f30bc92c6db89
adammendak <adam.mendak@gmail.com>
2018-09-25 20:21:46 +0200

    front-end

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from helpers.angular_dist_cleaner import add_static_to_angular_dist_files
+import os
 
-app = Flask(__name__)
+OS_PATH_DIR_NAME = str(os.path.join(os.path.dirname(__file__), "static/index.html"))
 
+app = Flask(__name__, static_url_path='/static')
+
+add_static_to_angular_dist_files(OS_PATH_DIR_NAME)
+
+
+#  todo dodac enkoding utf-8
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return send_from_directory('static', 'index.html')
+
+@app.route('/api/test')
+def test():
+    return "hello test"
 
 
 if __name__ == '__main__':
